aa48e1adb1c3c2b0a18901e7ad3bf25ec8cd2a05
Grammatopoulos Athanasios Vasileios <GramThanos@users.noreply.github.com>
2023-02-20 12:51:30 +0200

    removed dependencies and improved printing

--- R-CloudFlareBypasser.py
+++ R-CloudFlareBypasser.py
@@ -1,28 +1,35 @@
-import colorama , pyfiglet
-import socket , sys , os
+import socket, sys, os
 
-rd = colorama.Fore.RED
-mag = colorama.Fore.MAGENTA
-cv = colorama.Fore.WHITE
-gn = colorama.Fore.GREEN
-bl = colorama.Fore.LIGHTBLUE_EX
-yl = colorama.Fore.LIGHTGREEN_EX
-def banner():
-  fg = pyfiglet.Figlet(font="small").renderText("RemaxBox Team")
-  print (bl + fg)
-  print (rd + "CloudFlare bypasser , Get Real IP of website")
-  print (mag + "Created By Maximum Radikali")
+# Print banner
+print(''' ___                    ___            _____
+| _ \\___ _ __  __ ___ _| _ ) _____ __ |_   _|__ __ _ _ __
+|   / -_) '  \\/ _` \\ \\ / _ \\/ _ \\ \\ /   | |/ -_) _` | '  \\
+|_|_\\___|_|_|_\\__,_/_\\_\\___/\\___/_\\_\\   |_|\\___\\__,_|_|_|_|
+CloudFlare bypasser , Get Real IP of website
+Created By Maximum Radikali, modified by GramThanos
+''')
 
-banner()
-site = input(bl + "Please Enter a url ex : (google.com)-> ")
-f = open("dom.txt","r")
-print (yl + "Waiting ...")
-for i in f:
-  k = i.strip()
+domain = input("Please Enter a url ex : (google.com)-> ")
+
+try:
+  hostname = socket.gethostbyname(domain)
+  print('The public hostname is: ' + hostname + ' (' + domain + ')')
+except:
+  print('Failed to get the public hostname for ' + domain)
+
+subdomains = open("dom.txt","r")
+print("Checking subdomains ...")
+
+found = 0
+for subdomain in subdomains:
+  subdomain = subdomain.strip()
   try:
-    fs = socket.gethostbyname(k+site)
-    print (gn + fs + cv)
-    
+    hostname = socket.gethostbyname(subdomain + domain)
+    print('Found posible hostname: ' + hostname + ' (' + subdomain + domain + ')')
+    found += 1
   except:
     pass
-print ("The operation has been successfully :)")
+if found > 0:
+  print ("The operation has been successful :)")
+else:
+  print ("The operation has been unsuccessfully :(")
